e94d57b3584e88afa5092382959daf370cc9c010
Dzhalal Atabaev <djaldoit@gmail.com>
2024-06-01 14:37:13 +0300

    add jenkins 0.1

--- conftest.py
+++ conftest.py
@@ -2,7 +2,7 @@ import pytest
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selene import Browser, Config
+from selene import browser
 
 from utils import attach
 from dotenv import load_dotenv
@@ -21,22 +21,30 @@ selenoid_url = os.getenv("SELENOID_URL")
 
 @pytest.fixture(scope='function')
 def setup_browser(request):
+    browser_version = request.config.getoption('--browser_version')
+    browser_version = browser_version if browser_version != "" else DEFAULT_BROWSER_VERSION
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
-        "browserVersion": "100.0",
+        "browserVersion": browser_version,
         "selenoid:options": {
             "enableVNC": True,
             "enableVideo": True
         }
     }
     options.capabilities.update(selenoid_capabilities)
+
+    login = os.getenv('LOGIN')
+    password = os.getenv('PASSWORD')
+
     driver = webdriver.Remote(
-        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
         options=options
     )
 
-    browser = Browser(Config(driver))
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
+    browser.config.driver = driver
 
     yield browser
 
